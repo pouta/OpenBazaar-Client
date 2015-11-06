5efd05648b45b49be94031a57a26339ba6227503
pouta <pedromamoura@hotmail.com>
2015-11-06 17:25:33 +0000

    Fixed merge

--- js/views/pageNavVw.js
+++ js/views/pageNavVw.js
@@ -67,8 +67,6 @@ module.exports = Backbone.View.extend({
     this.render();
   },
 
-<<<<<<< HEAD
-=======
   handleSocketMessage: function(response) {
     "use strict";
     var data = JSON.parse(response.data);
@@ -76,48 +74,7 @@ module.exports = Backbone.View.extend({
       console.log(data);
     }
   },
-
-  initAccordion: function(targ){
-    "use strict";
-    var acc = $(targ);
-    var accWidth = acc.width();
-    var accHeight = acc.height();
-    var accChildren = acc.find('.accordion-child');
-    var accNum = accChildren.length;
-    var accWin = acc.find('.accordion-window');
-
-    accWin.css({'left':0, 'width': function(){return accWidth * accNum;}});
-    accChildren.css({'width':accWidth, 'height':accHeight});
-    acc.find('.js-accordionNext').on('click', function(){
-      var oldPos = accWin.css('left').replace("px","");
-      if(oldPos > (accWidth * accNum * -1 + accWidth)){
-        accWin.css('left', function(){
-          return parseInt(accWin.css('left').replace("px","")) - accWidth;
-        });
-      }
-      // focus search input
-      $(this).closest('.accordion-child').next('.accordion-child').find('.search').focus();
-    });
-    acc.find('.js-accordionPrev').on('click', function(){
-      var oldPos = accWin.css('left').replace("px","");
-      if(oldPos < (0)){
-        accWin.css('left', function(){
-          return parseInt(accWin.css('left').replace("px","")) + accWidth;
-        });
-      }
-    });
-    //set up filterable lists.
-    //TODO: this is terrible, change to run when an event is emmitted from the subviews marking them as done rendering
-    setTimeout(function(){
-      var List = window.List;
-      var countryList = new List('homeModal-countryList', {valueNames: ['homeModal-country']});
-      var currencyList = new List('homeModal-currencyList', {valueNames: ['homeModal-currency']});
-      var timeList = new List('homeModal-timeList', {valueNames: ['homeModal-time']});
-      var languageList = new List('homeModal-languageList', {valueNames: ['homeModal-language']});
-    }, 1000);
-  },
-
->>>>>>> upstream/master
+  
   closeNav: function(){
     "use strict";
     var targ = this.$el.find('.js-navProfileMenu');
